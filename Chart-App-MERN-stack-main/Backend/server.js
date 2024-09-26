bc0c9d650496a3ef4f707805199c27270e09b3ff
Coder-g4u <105904276+Coder-g4u@users.noreply.github.com>
2024-09-26 22:42:29 +0530

    chart-full

---
+++ Chart-App-MERN-stack-main/Backend/server.js
@@ -0,0 +1,41 @@
+import path from 'path';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import meassageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js";
+
+import connectTOMongoDB from "./db/connectToMongoDB.js";
+import { app,server } from "./socket/socket.js";
+
+ 
+ const PORT=process.env.PORT || 5000;
+
+ const __dirname=path.resolve();
+
+ dotenv.config(); 
+
+ app.use(express.json());
+ app.use(cookieParser());
+
+ app.use("/api/auth",authRoutes);
+ app.use("/api/messages",meassageRoutes);
+ app.use("/api/users",userRoutes);
+
+ app.use(express.static(path.join(__dirname,"/Frontend/dist")));
+
+ app.get("*",(req,res)=>{
+   res.sendFile(path.join(__dirname,"Frontend","dist","index.html"));
+ });
+
+//  app.get("/",(req,res)=>{
+//     res.send("env");
+//  });
+
+
+ server.listen(PORT,()=>{
+    connectTOMongoDB();
+    console.log(`sever is running on port ${PORT}`);
+});
---
+++ Chart-App-MERN-stack-main/Frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Conversation from './Conversation.jsx'
+import useGetconversations from '../../hooks/useGetconversations.js'
+import { getRandomEmoji } from '../../utils/emojis.js';
+
+const Conversations = () => {
+  const {loading,conversations}= useGetconversations();
+  
+  return (
+    <div className='py-2 flex flex-col overflow-auto'>
+       {conversations.map((conversation,idx)=>(
+        <Conversation
+           key={conversation._id}
+           conversation={conversation}
+           emoji={getRandomEmoji()}
+           lastIdx={idx==conversations.length-1}
+        />
+       ))
+       }
+        {loading ? <span className='loading loading-spinner mx-auto'></span> : null}
+    </div>
+  )
+}
+
+export default Conversations
